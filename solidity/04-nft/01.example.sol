bc687225d6d96fe39a6f61db92d4555288af9d77
louis <jyxiaohe@gmail.com>
2024-06-19 15:18:30 +0800

    test

---
+++ solidity/04-nft/01.example.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.17;
+
+contract Example{
+		// 定义 A 结构体，用于存储 Apple 的信息
+    struct A {
+        string name;        //A 名称
+        string description; //A 描述信息
+        address owner;      //A 所有者地址
+    } 
+}
